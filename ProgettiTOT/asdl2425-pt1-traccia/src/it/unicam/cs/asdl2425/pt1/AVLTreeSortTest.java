660473a45ba7cb355ff3c71dd80c2479477a07fb
Peppuzzo <giuseppe.43g@libero.it>
2025-02-06 18:33:49 +0100

    Delete import in AVLTreeSortTest

--- ProgettiTOT/asdl2425-pt1-traccia/src/it/unicam/cs/asdl2425/pt1/AVLTreeSortTest.java
+++ ProgettiTOT/asdl2425-pt1-traccia/src/it/unicam/cs/asdl2425/pt1/AVLTreeSortTest.java
@@ -7,9 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * @author Luca Tesei (template) GIUSEPPE, CALABRESE
  * giusepp.calabrese@studenti.unicam.it (implementazione)
